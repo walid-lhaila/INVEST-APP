836395f8b1f4f2b7390c094453fade7036193ec8
walid lhaila <walidlhaila00@gmail.com>
2025-02-24 14:30:32 +0000

    Feat: CREATE INTERFACE TO DISPLAY ALL CONVERSATION  OF THE USER

--- app/(tab)/chat.tsx
+++ app/(tab)/chat.tsx
@@ -1,12 +1,64 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {ScrollView, StatusBar, StyleSheet, Text, View} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import ChatSearch from "@/app/Components/ChatSearch";
+import ConversationsCard from "@/app/Components/ConversationsCard";
 
-function Chat(props) {
+function Chat() {
     return (
-        <View>
-            <Text>Chat</Text>
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <StatusBar translucent backgroundColor="transparent" />
+            <LinearGradient colors={['#77a6f7', '#f6f7ff']} style={styles.gradient}>
+                <View style={styles.content}>
+                    <Text style={{ fontSize: 35, fontWeight: 700, color: 'white'}}>Chat</Text>
+                    <ChatSearch />
+                    <Text style={{ marginTop: 20, color: 'white', fontWeight: 700,}}>ALL MESSAGES</Text>
+                    <ScrollView  showsVerticalScrollIndicator={false} keyboardShouldPersistTaps="handled" nestedScrollEnabled={true} style={{ marginTop: 10}}>
+                        <ConversationsCard name="Walid Lhaila" count="2" message="Hello, How Are You" time='08:25' />
+                        <ConversationsCard name="Ahmed Hounati" count="3" message="Hello, How Are You" time='08:25' />
+                        <ConversationsCard name="Brahim Ouborrih" count="1" message="Hello, How Are You" time='08:25' />
+                        <ConversationsCard name="Imad Esaghir" count="4" message="Hello, How Are You" time='08:25' />
+                        <ConversationsCard name="Mohamed Joual" count="4" message="Hello, How Are You" time='08:25' />
+                    </ScrollView>
+                </View>
+            </LinearGradient>
         </View>
     );
 }
 
 export default Chat;
+
+const styles = StyleSheet.create({
+    gradient: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        height: '100%',
+    },
+    content: {
+        paddingHorizontal: 15,
+        paddingVertical: 35,
+        flex: 1,
+        top: 30,
+        width: '100%'
+    },
+    profileContainer: {
+        width: 50,
+        backgroundColor: 'white',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 25,
+        overflow: 'hidden',
+    },
+});
---
+++ app/Components/ConversationsCard.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {ImageBackground, StyleSheet, Text, View} from "react-native";
+import profile from "@/assets/images/profile.png";
+
+function ConversationsCard({name, message, count, time}) {
+    return (
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderBottomColor: "#9c9c9c", borderWidth: 1, borderBottomWidth: 1, borderLeftWidth: 0, borderRightWidth: 0, borderTopWidth: 0}}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10, paddingVertical: 10}}>
+                <View style={styles.profileContainer}>
+                    <ImageBackground
+                        style={styles.profileImage}
+                        source={profile}
+                        resizeMode="cover"
+                    />
+                </View>
+
+                <View>
+                    <Text style={{color: 'black', fontSize: 19, fontWeight: 700, fontFamily: "Roboto"}}>{name}</Text>
+                    <View style={{flexDirection: 'row', alignItems: 'center', gap: 5, paddingHorizontal: 5, paddingVertical: 2, borderRadius: 10}}>
+                        <Text style={{color: '#717171', fontSize: 14, fontWeight: 300, fontFamily: "serif"}}>{message}</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={{ alignItems: 'center', gap: 5}}>
+                <Text style={{ color: '#717171'}}>{time}</Text>
+                <View style={{backgroundColor: 'white', paddingVertical: 1, paddingHorizontal: 5, borderRadius: 50}}>
+                    <Text>{count}</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+export default ConversationsCard;
+
+const styles = StyleSheet.create({
+    profileContainer: {
+        backgroundColor: 'white',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+        elevation: 5,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+    },
+    profileImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        overflow: 'hidden',
+    },
+
+});
