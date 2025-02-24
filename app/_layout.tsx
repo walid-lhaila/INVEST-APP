f9767cbfb2f9492bad0faa53f5261cddb7fc320d
walid lhaila <walidlhaila00@gmail.com>
2025-02-24 16:40:21 +0000

    Feat: CREATE INTERFACE TO DISPLAY MESSAGES BETWEEN USERS

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ export default function RootLayout() {
       <Stack screenOptions={{gestureEnabled: false}}>
           <Stack.Screen  name="(tab)" options={{ headerShown: false }} />
           <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="chatComponent" options={{ headerShown: false }} />
           <Stack.Screen name="registerSelect" options={{ headerShown: false }} />
           <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="investorRegister" options={{ headerShown: false }} />
