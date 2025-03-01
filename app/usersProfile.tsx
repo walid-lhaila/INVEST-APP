152d9bfdc9a2b9e327ad60b78662f46cbabfd947
walid lhaila <walidlhaila00@gmail.com>
2025-03-01 13:18:35 +0000

    Feat: DISPLAY USERS PROFILE DETAILS

---
+++ app/usersProfile.tsx
@@ -0,0 +1,132 @@
+import React, {useEffect} from 'react';
+import {ActivityIndicator, ImageBackground, ScrollView, StyleSheet, Text, View} from "react-native";
+import profile from "@/assets/images/profile.png";
+import ProjectRealizedCard from "@/app/Components/ProjectRealizedCard";
+import {useLocalSearchParams} from "expo-router";
+import useGetUserByUsername from "@/app/hooks/useGetUserByUsername";
+
+function UsersProfile() {
+    const {username} = useLocalSearchParams();
+    const {user, isLoading, getUserByUsername} = useGetUserByUsername();
+
+    useEffect(() => {
+        if(username) {
+            getUserByUsername(username);
+        }
+    }, [username]);
+    if(isLoading){
+        return <ActivityIndicator size="large" color="#77a6f7" style={styles.loading} />;
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.profileWrapper}>
+                    <ImageBackground
+                        style={styles.profileImage}
+                        source={profile}
+                        resizeMode="cover"
+                    />
+                </View>
+                <Text style={styles.name}>{user.firstName} {user.lastName}</Text>
+                <Text style={styles.email}>{user.email}</Text>
+            </View>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <Text style={styles.sectionTitle}>About</Text>
+                <View style={styles.card}>
+                    <DetailItem label="USERNAME" value={user.username} color="#77a6f7" />
+                    <DetailItem label="MOBILE" value={user.phone} />
+                    <DetailItem label="SERVICE" value={user.services} />
+                </View>
+
+                    <>
+                        <Text style={styles.sectionTitle}>Company</Text>
+                        <View style={styles.card}>
+                            <DetailItem label="NAME" value={user.companyName} />
+                            <DetailItem label="DESCRIPTION" value={user.companyDescription} />
+                        </View>
+                    </>
+
+
+                <Text style={styles.sectionTitle}>Project Realized</Text>
+                <View style={{paddingVertical: 10}}>
+                        <ProjectRealizedCard tags="tes" role="Investor" description="trev dec erfsdvreq vqsdcezefr gfdsdezd " title="test" user="test" budget="7654" endDate="10" startDate="3232" />
+                </View>
+            </ScrollView>
+
+        </View>
+    );
+}
+
+const DetailItem = ({ label, value, color = "black", }) => (
+    <View style={styles.detailItem}>
+        <Text>{label}</Text>
+        <Text style={{ color, paddingVertical: 3 }}>{value}</Text>
+    </View>
+);
+
+export default UsersProfile;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#f7f7f7",
+    },
+    header: {
+        backgroundColor: "#77a6f7",
+        borderRadius: 25,
+        paddingVertical: 45,
+        alignItems: "center",
+    },
+    profileWrapper: {
+        backgroundColor: "white",
+        borderRadius: 50,
+        padding: 5,
+        elevation: 5,
+    },
+    profileImage: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        overflow: "hidden",
+    },
+    name: {
+        color: "white",
+        fontSize: 22,
+        fontWeight: "bold",
+        marginTop: 10,
+    },
+    email: {
+        color: "white",
+        fontSize: 16,
+        opacity: 0.8,
+    },
+    sectionTitle: {
+        paddingHorizontal: 10,
+        fontSize: 22,
+        fontWeight: "600",
+        marginVertical: 15,
+        color: "#333",
+    },
+    card: {
+        backgroundColor: "white",
+        borderRadius: 15,
+        padding: 15,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+    },
+    detailItem: {
+        marginBottom: 12,
+        paddingBottom: 8,
+        borderBottomWidth: 1,
+        borderBottomColor: "rgba(0,0,0,0.1)",
+    },
+    loading: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
