74f7bef405dc198ba8a19561f3d57a1a95606156
walid lhaila <walidlhaila00@gmail.com>
2025-03-03 17:21:09 +0000

    Feat: CONSOMME ENDPOINT SEND REQUEST

--- app/redux/slices/RequestSlice.ts
+++ app/redux/slices/RequestSlice.ts
@@ -39,6 +39,29 @@ export const getAllRequest = createAsyncThunk(
 );
 
 
+export const sendRequest = createAsyncThunk(
+    'request/sendRequest',
+    async(requestData: {receiver: string}, {rejectWithValue}) => {
+        try {
+            const storedToken = await AsyncStorage.getItem('token');
+            if(!storedToken) {
+                return rejectWithValue('Authorization token is missing');
+            }
+            const response = await axios.post(`${process.env.EXPO_PUBLIC_BACKEND_URL}/requests/send`, requestData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${storedToken}`,
+                    },
+                },
+                );
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error?.response?.data?.message || 'Something Went Wrong');
+        }
+    }
+)
+
+
 const requestSlice = createSlice({
     name: 'request',
     initialState,
@@ -54,6 +77,19 @@ const requestSlice = createSlice({
                 state.requests = action.payload;
             })
             .addCase(getAllRequest.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload as string;
+            })
+
+            .addCase(sendRequest.pending, (state) => {
+                state.isLoading = true;
+                state.error = false;
+            })
+            .addCase(sendRequest.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.requests = [action.payload, ...state.requests];
+            })
+            .addCase(sendRequest.rejected, (state, action) => {
                 state.isLoading = false;
                 state.error = action.payload as string;
             });
