9285479c6cdce20f6153b3283f1b15b897f868fd
walid lhaila <walidlhaila00@gmail.com>
2025-03-05 10:19:00 +0000

    Feat: CONSOMME EDNPOINT TO MAKE USER ADD A POST IN FAVORITES

--- app/(tab)/(user)/favorites.tsx
+++ app/(tab)/(user)/favorites.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import {ActivityIndicator, StatusBar, StyleSheet, Text, View} from "react-native";
 import useGetFavorites from "@/app/hooks/useGetFavorites";
 import {LinearGradient} from "expo-linear-gradient";
-import UserPostCard from "@/app/Components/UserPostCard";
 import useRemoveFavorites from "@/app/hooks/useRemoveFavorites";
 import {Toast} from "@/app/CustomToast";
+import FavoriteCard from "@/app/Components/FavoriteCard";
 
 function Favorites() {
-    const {favorites, isLoading} = useGetFavorites();
+    const {favorites} = useGetFavorites();
     const {handleDelete} = useRemoveFavorites();
-    if (isLoading) {
-        return <ActivityIndicator size="large" color="#77a6f7" style={styles.loader} />;
-    }
+
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
             <StatusBar translucent backgroundColor="transparent" />
@@ -19,11 +17,10 @@ function Favorites() {
                 <View style={styles.content}>
                     <Text style={styles.header}>My Favorites</Text>
                     {favorites.map((favorite) => (
-                        <UserPostCard onDelete={() => handleDelete(favorite._id)} key={favorite._id} title={favorite.post.title} description={favorite.post.description} category={favorite.post.category} location={favorite.post.location} currentInvestment={favorite.post.currentInvestment} investmentGoal={favorite.post.investmentGoal} entrepreneur={favorite.post.entrepreneur} createdAt={favorite.post.createdAt} />
+                        <FavoriteCard  onDelete={() => handleDelete(favorite._id)} key={favorite._id} title={favorite.post.title} description={favorite.post.description} category={favorite.post.category} location={favorite.post.location} currentInvestment={favorite.post.currentInvestment} investmentGoal={favorite.post.investmentGoal} entrepreneur={favorite.post.entrepreneur} createdAt={favorite.post.createdAt} />
                     ))}
                 </View>
             </LinearGradient>
-            <Toast />
         </View>
     );
 }
