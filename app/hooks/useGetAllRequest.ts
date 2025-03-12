bd0f1d4396b4ad0a02876f291c82acd9228cb4bc
walid lhaila <walidlhaila00@gmail.com>
2025-03-12 12:09:47 +0000

    Feat: HANDLEEXISTSING REQUESTS ANS EXISTING POSTS FAVORIETS

--- app/hooks/useGetAllRequest.ts
+++ app/hooks/useGetAllRequest.ts
@@ -1,12 +1,12 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getAllRequest} from "@/app/redux/slices/RequestSlice";
+import {usePathname} from "expo-router";
 
 
 const useGetAllRequest = () => {
     const dispatch = useDispatch();
     const {requests, isLoading, error} =  useSelector((state) => state.request);
-
     useEffect(() => {
         dispatch(getAllRequest());
     }, [dispatch]);
